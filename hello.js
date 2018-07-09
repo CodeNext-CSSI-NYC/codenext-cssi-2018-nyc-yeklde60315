67f8461e2a84dcb665bc30d4ebeea75253378c90
Yenry <yenry.simon@csedge.org>
2018-07-09 16:36:48 -0400

    math workout in termal and atom

--- hello.js
+++ hello.js
@@ -1,1 +1,2 @@
 // Author: FirstName LastName
+console.log("NO,it's"+(1+1)+)
--- math-practice.js
+++ math-practice.js
@@ -1,3 +1,4 @@
+//Authoor: Yenry Simon
 // Under each question, write the code that solves it, and then write your answer in a comment
 // For example, if the question is: What's the sum of 79 and 2018 you would write:
 console.log(79 + 2018);
@@ -9,12 +10,15 @@ const q = 60;
 const r = 25;
 
 // 1. Is 91 divisible by 13? (The answer should be either true or false)
-
+console.log(91%13==0);
+//true
 // 2. What is the result when the q divided by r is rounded to the nearest integer?
-
+console.log(Math.round(q/r));
+//2
 // 3. Is the product of p and r greater than q squared? (The answer should be either true or false)
-
+console.log((p*r)<(q**2));
 // 4. What is the remainder when the sum of q and r is divided by p?
+console.log(q*r%p);
 
 // 5. 27 squared is equal to what number raised to the third power?
 
