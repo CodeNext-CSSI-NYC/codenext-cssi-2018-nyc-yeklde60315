fd2ac62bb51c1c107cb3e8280b032695331bb26f
Yenry <yenry.simon@csedge.org>
2018-07-10 16:19:19 -0400

    math equation with short cut

---
+++ shortcuts-practice.js
@@ -0,0 +1,22 @@
+//Author: Yenry Simon
+
+// 1. Decrease the variable below by 1 using two different shortcuts.
+// Print out its value after each step to make sure it worked.
+let a = 7;
+console.log(a-= 1);
+console.log(a--);
+
+// // 2. Double the value of the variable below using a multiplication shortcut.
+// // Print out its value before and after each step to make sure it worked.
+let b =18;
+console.log(b*=2);
+// // 3. Predict the value of d. Then print it out to see if you were right.
+let c = 13;
+let d = c++;
+console.log(d);
+//the value will be 13
+
+// // 4. Move the decimal two spots to the left using a shortcut.
+// // Print out the value of e before and after to make sure it worked.
+let e = 234.5;
+console.log(e*=.01);
