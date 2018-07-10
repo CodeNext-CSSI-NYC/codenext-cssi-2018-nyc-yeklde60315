ff5953014a0b51ca27caf0cd2cd29f82196d54b2
Yenry <yenry.simon@csedge.org>
2018-07-10 10:38:31 -0400

    I wrote my name and then it said hello back to me

--- hello.js
+++ hello.js
@@ -1,2 +1,5 @@
-// Author: FirstName LastName
-console.log("NO,it's"+(1+1)+)
+// Author: Yenry Simon
+var readline = require("readline-sync");
+console.log("NO,it's"+(1+1));
+var userName = readline.question("Please enter your name: ");
+console.log("Hello " + userName + "!");
